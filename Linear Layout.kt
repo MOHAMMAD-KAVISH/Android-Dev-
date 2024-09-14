bd6d09b037e6898196bb617242146f3437deaf50
MOHAMMAD KAVISH <119128166+MOHAMMAD-KAVISH@users.noreply.github.com>
2024-09-14 22:58:57 +0530

    Update Linear Layout.kt

--- Linear Layout.kt
+++ Linear Layout.kt
@@ -1,32 +1,17 @@
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        //val i1 =findViewById<ImageView>(/* id = */ R.id.i1)
-        val showButton:Button = findViewById(R.id.b1)
-
-        val editText:EditText = findViewById(R.id.e1)
-
-        val textView:TextView = findViewById(R.id.t1)
-        showButton.setOnClickListener {
-            // Get the text from the EditText
-            val enteredText = editText.text.toString()
-
-            // Check if text is not empty
-            if (enteredText.isNotEmpty()) {
-                // Set the text in the TextView
-                if (enteredText.equals("Tanya", ignoreCase = true)) {
-                    textView.text = "Assalam walekum"
-                } else {
-                    // General greeting for other names
-                    textView.text = "Hello, Your Name is $enteredText! "
-                }
-            } else {
-                // Show a default message if the EditText is empty
-                textView.text = "Please enter your name!"
-            }
-        }
+//        val username=findViewById<EditText>(R.id.editTextText)
+//        val password=findViewById<EditText>(R.id.editTextNumberPassword)
+//        val output=findViewById<TextView>(R.id.textView)
+//        val btn=findViewById<Button>(R.id.button2)
+//        btn.setOnClickListener{
+//            val a=username.text.toString()
+//            val b=password.text.toString()
+//            output.text= "My name is "+a+" and password is "+b
+//        }
     }
 }
